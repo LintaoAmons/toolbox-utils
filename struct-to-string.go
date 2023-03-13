7c30d136b2c82891c634ca2291ef067f6347735d
Lintao <lintao.amons@outlook.com>
2023-03-13 17:47:29 +0800

    format time.Time

--- struct-to-string.go
+++ struct-to-string.go
@@ -4,17 +4,32 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+	"time"
 )
 
 func StructToString(s interface{}) string {
+	// Get the reflect.Value of the struct
 	value := reflect.ValueOf(s)
+
 	var parts []string
 
+	// Loop over the fields of the struct
 	for i := 0; i < value.NumField(); i++ {
+		// Get the reflect.Value of the field
 		fieldValue := value.Field(i)
-		fieldString := fmt.Sprintf("'%v'", fieldValue.Interface())
-		parts = append(parts, fieldString)
+
+		// Check if the field type is time.Time
+		if fieldValue.Type() == reflect.TypeOf(time.Time{}) {
+			// Format the time.Time field as a string
+			fieldString := fmt.Sprintf("'%s'::timestamp WITH TIME ZONE", fieldValue.Interface().(time.Time).Format("2006-01-02 15:04:05.999999 -07:00"))
+			parts = append(parts, fieldString)
+		} else {
+			// Convert the field value to a string and add it to the parts slice
+			fieldString := fmt.Sprintf("'%v'", fieldValue.Interface())
+			parts = append(parts, fieldString)
+		}
 	}
 
+	// Join the parts slice into a comma-separated string
 	return strings.Join(parts, ", ")
 }
--- struct-to-string_test.go
+++ struct-to-string_test.go
@@ -1,33 +1,35 @@
 package toolboxutils_test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	toolboxutils "github.com/LintaoAmons/toolbox-utils"
 )
 
-type TestStruct struct {
-	Field1 string
-	Field2 string
-	Field3 string
-}
-
-func TestStructToString(t *testing.T) {
-	TestStruct := newTestStruct()
-
-	result := toolboxutils.StructToString(TestStruct)
-
-	expected := "'field1', 'field2', 'field3'"
-
-	if result != expected {
-		t.Errorf("unexpected result: %s, expected: %s", result, expected)
+func TestStructToStringWithTime(t *testing.T) {
+	// Define a test struct that includes a time.Time field
+	type MyStruct struct {
+		ID        int
+		Name      string
+		CreatedAt time.Time
 	}
-}
 
-func newTestStruct() TestStruct {
-	return TestStruct{
-		Field1: "field1",
-		Field2: "field2",
-		Field3: "field3",
+	// Create a test struct instance with a time.Time value
+	createdAt := time.Date(2022, 3, 13, 12, 0, 0, 0, time.UTC)
+	s := MyStruct{
+		ID:        123,
+		Name:      "Test",
+		CreatedAt: createdAt,
+	}
+
+	// Call the StructToString function with the test struct
+	result := toolboxutils.StructToString(s)
+
+	// Check that the result includes the formatted time.Time field value in the correct format
+	expected := fmt.Sprintf("'%d', '%s', '%s'::timestamp WITH TIME ZONE", s.ID, s.Name, createdAt.Format("2006-01-02 15:04:05.999999 -07:00"))
+	if result != expected {
+		t.Errorf("StructToString returned %q, expected %q", result, expected)
 	}
 }
